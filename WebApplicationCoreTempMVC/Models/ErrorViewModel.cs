7255a23076dd2b25fc418ed99d7920360333e6f5
LazizEx <laziz@mail.ru>
2018-01-16 19:16:38 +0300

    Добавьте файлы проекта.

---
+++ WebApplicationCoreTempMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationCoreTempMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
